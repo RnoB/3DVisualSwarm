c50e892423c1bb29ff22a6402836fc46c25359a4
rnoB <rno.bstin@gmail.com>
2023-12-19 13:38:04 +0100

    updated percent simulations

--- src/multiSimulator.py
+++ src/multiSimulator.py
@@ -74,8 +74,11 @@ def startSimulation(repId):
             for k in range(0,N):
                 sx = 10**(repId["sx"]*2*(random.random()-.5))
                 sim.setScale(sx,sx,sx,k)
-        elif repId["mode"] == 3:
-            nScale = int(.2*N)
+        elif repId["mode"] == 3 and repId["p0"]>0:
+
+            nScale = int(repId["p0"]*N)
+            if nScale<1:
+                nScale = 1
             for k in range(0,nScale):
                 sim.setScale(repId["sx"],repId["sx"],repId["sx"],k)
         
