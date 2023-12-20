c000837d17a909355d06fc6b0a324d16fc1b7dd7
rnoB <rno.bstin@gmail.com>
2023-12-20 11:38:33 +0100

    added some parameters

--- src/analyzer/server/malkoDB/urls.py
+++ src/analyzer/server/malkoDB/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     re_path(r'^(?P<project>[\w\-]+)/$', views.ProjectView.as_view(), name='indexProject'),
     re_path(r'^(?P<project>[\w\-]+)/(?P<experiment>[\w\-]+)/$', views.ExperimentView.as_view(), name='indexExperiment'),
-    re_path(r'^(?P<project>[\w\-]+)/(?P<experiment>[\w\-]+)/(?P<p0>[\w\-]+)/(?P<value0>[0-9]+)/$', views.ExperimentView.as_view(), name='indexExperiment'),
+    re_path(r'^(?P<project>[\w\-]+)/(?P<experiment>[\w\-]+)/(?P<p0>[\w\-]+)/(?P<value0>[\w\-]+)/$', views.ExperimentView.as_view(), name='indexExperiment'),
     #re_path(r'^(?P<project>[\w\-]+)/(?P<N>[0-9]+)/$', views.VisNView.as_view(), name='VisN'),
 ]
