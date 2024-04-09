f2f7d7b4199462592acfe77e934b9a8a5ba27c74
rnoB <rno.bstin@gmail.com>
2024-04-09 14:19:46 +0200

    parralelized analysis

--- src/analyzer/analyzer.py
+++ src/analyzer/analyzer.py
@@ -32,6 +32,8 @@ import scipy.spatial as sc
 import json
 import multiprocessing 
 
+anal = dbFiller.Analyzer()
+
 def angleDifference(A1, A2):
     A = A1 - A2
     A = (A + np.pi) % (2 * np.pi) - np.pi
@@ -84,6 +86,29 @@ def getAllDistance(X):
     distance = {"mean" : np.array(dMean),"min" : np.array(dMin),"max" : np.array(dMax),"minMean" : np.array(dMinMean),"maxMean" : np.array(dMaxMean)}
     return distance
 
+def analSim(simId,step = 10):
+    repId = simId[0]
+    parameters = anal.getParameters(simId,project,exp)
+    X = anal.getDataSet(repId)
+    path = anal.getDataPath(repId)
+    N = parameters["N"]
+    mode = parameters["mode"]
+    center = centerOfMassSpeed(X,step = step)
+    distance = getAllDistance(X)
+    pol = polarization(X)
+    dataDistance = {"mean" : np.mean(distance["mean"][-1000:]),
+                    "min" : np.mean(distance["min"][-1000:]),
+                    "max" : np.mean(distance["max"][-1000:]),
+                    "minMean" : np.mean(distance["minMean"][-1000:]),
+                    "maxMean" : np.mean(distance["maxMean"][-1000:])}
+    dataCenter = {"v" : np.mean(center["v"][-1000:]),
+                  "dphi" : np.mean(center["dphi"][-1000:])}
+    group = {"polarization" : np.mean(pol[-1000:])}
+    data = {"distance" : dataDistance,"center" : dataCenter,"group" : group}
+    with open(path+"/globalData.json","w") as f:
+        json.dump(data,f)  
+
+
 
 class Analyzer:
 
@@ -121,51 +146,33 @@ class Analyzer:
                             with open(path+"/globalData.json","w") as f:
                                 json.dump(data,f)
 
-    def analSim(simId):
-        repId = simId[0]
-        parameters = self.anal.getParameters(simId,project,exp)
-        X = self.anal.getDataSet(repId)
-        path = self.anal.getDataPath(repId)
-        N = parameters["N"]
-        mode = parameters["mode"]
-        center = centerOfMassSpeed(X,step = self.step)
-        distance = getAllDistance(X)
-        pol = polarization(X)
-        dataDistance = {"mean" : np.mean(distance["mean"][-1000:]),
-                        "min" : np.mean(distance["min"][-1000:]),
-                        "max" : np.mean(distance["max"][-1000:]),
-                        "minMean" : np.mean(distance["minMean"][-1000:]),
-                        "maxMean" : np.mean(distance["maxMean"][-1000:])}
-        dataCenter = {"v" : np.mean(center["v"][-1000:]),
-                      "dphi" : np.mean(center["dphi"][-1000:])}
-        group = {"polarization" : np.mean(pol[-1000:])}
-        data = {"distance" : dataDistance,"center" : dataCenter,"group" : group}
-        with open(path+"/globalData.json","w") as f:
-            json.dump(data,f)  
+
 
 
     def start(self):
         repIds = []
         for project in self.projects:
-            self.experiments = self.anal.getExperiments(project)
+            self.experiments = anal.getExperiments(project)
             for exp in self.experiments:
-                sortingKeys,sortedKeys = self.anal.getExperimentSortingKeys(project,exp)
+                sortingKeys,sortedKeys = anal.getExperimentSortingKeys(project,exp)
                 for key in sortedKeys:
                     
-                    simId = self.anal.getSimIds(key)
+                    simId = anal.getSimIds(key)
                     
                     if len(simId)>0:
                         simId = simId[0][0]
-                        repIds.extend(self.anal.getRepIds(simId))
-        print(repIds)
+                        repIds.extend(anal.getRepIds(simId))
+        
         pool = multiprocessing.Pool(processes=self.nThreads)
-        pool.map_async(self.analSim, repIds)
+        pool.map_async(analSim, repIds)
         pool.close()
         pool.join()
     
 
     def __init__(self,step = 10,nThreads = 2):
+        #this step number does not get into the multiprocessing pool and this is going to be
+        #problematice. 
         self.step = step
-        self.anal = dbFiller.Analyzer()
+    
         self.projects = self.anal.projects
         self.nThreads = nThreads
